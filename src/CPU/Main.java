02b16faf3df97e092aef5c3cc8d171aaf6eccc35
SirGoody <chasegoodmanbsu@gmail.com>
2016-11-03 12:39:04 -0400

    Starts the GUI in Main class

--- src/CPU/Main.java
+++ src/CPU/Main.java
@@ -4,12 +4,27 @@ package CPU;
 
 
 import javafx.application.Application;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.event.*;
 
-public class Main{
+public class Main extends Application{
 
     public static void main(String[] args){
+    launch(args);
+    }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("CS 376 CPU Scheduling");
+
+        StackPane layout = new StackPane();
+
+        Scene scene = new Scene(layout, 500, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
 }
