a2dc0158543a0f8c09c417160b27db39d8200d91
SirGoody <chasegoodmanbsu@gmail.com>
2016-11-03 12:28:11 -0400

    Starts the GUI in Main class

---
+++ src/CPU/Main.java
@@ -0,0 +1,15 @@
+package CPU;
+
+//Spencer Kolbus and Chase Goodman created on Nov 3rd, 2016
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main{
+
+    public static void main(String[] args){
+
+    }
+
+}
--- src/CPU/main.java
+++
@@ -1,9 +0,0 @@
-package CPU;
-
-//Spencer Kolbus and Chase Goodman created on Nov 3rd, 2016
-
-public class main {
-
-
-
-}
