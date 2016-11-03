0b09a27c13b097fa06075a6547f51a0d742249c5
SirGoody <chasegoodmanbsu@gmail.com>
2016-11-03 13:01:18 -0400

    Starts the GUI, still work in progress

--- src/CPU/Main.java
+++ src/CPU/Main.java
@@ -1,9 +1,13 @@
 package CPU;
 
-//Spencer Kolbus and Chase Goodman created on Nov 3rd, 2016
+/* Spencer Kolbus and Chase Goodman created on Nov 3rd, 2016 */
 
 
 import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.*;
@@ -20,7 +24,23 @@ public class Main extends Application{
 
         primaryStage.setTitle("CS 376 CPU Scheduling");
 
-        StackPane layout = new StackPane();
+        BorderPane layout = new BorderPane();
+
+        Button done = new Button();
+        TextField jobID = new TextField();
+        TextField burstTime = new TextField();
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
+
+        //getItems returns the ObservableList object which you can add items to
+        choiceBox.getItems().addAll("Round Robin", "First Come First Serve", "Shortest Job First");
+
+        //Set a default value
+        choiceBox.setValue("Round Robin");
+
+        layout.setLeft(jobID);
+        layout.setCenter(burstTime);
+        layout.setBottom(done);
+        layout.setTop(choiceBox);
 
         Scene scene = new Scene(layout, 500, 250);
         primaryStage.setScene(scene);
